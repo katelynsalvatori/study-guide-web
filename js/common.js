1287f1e32d6b848faa16704629b5f01beeefe05a
Katelyn Salvatori <katelyn.salvatori@gmail.com>
2018-12-26 11:10:16 -0500

    Removed files generated from upgrading Django

--- js/common.js
+++
@@ -1,4 +0,0 @@
-function toggleDisplay(displayId) {
-    var currDisplay = document.getElementById(displayId).style.display;
-    document.getElementById(displayId).style.display = currDisplay === "none" ? "" : "none";
-}
