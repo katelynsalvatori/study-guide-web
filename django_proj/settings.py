cbcd0a58c97f86874dbbaeed1e73b07b3da279f2
Katelyn Salvatori <katelyn.salvatori@gmail.com>
2018-12-26 09:12:19 -0500

    Set static root for deployment

--- django_proj/settings.py
+++ django_proj/settings.py
@@ -1,4 +1,5 @@
 # Django settings for django_proj project.
+import os
 
 DEBUG = True
 # TEMPLATE_DEBUG = DEBUG
@@ -60,7 +61,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
